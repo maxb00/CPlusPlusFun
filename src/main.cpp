079e88c2cd9f2d950ee1994808c70aa5ce761da6
maxb00 <maxbar00@yahoo.com>
2015-01-20 09:58:36 -0500

    Added loops

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,33 @@ using namespace std;
 int main()
 {
   int sides;
+  bool run = true;
+  bool answering;
+  char again;
 
   mt19937 randomGenerator (time(0));
   
-  cout << "Welcome!\n\n How many sides?" << endl;
-  cin >> sides
+  cout << "Welcome!\n\n";
+  while(run){
+    cout << "How many sides?" << endl;
+    cin >> sides;
   
-  uniform_int_distribution diceRoll(1, sides);
+    uniform_int_distribution<int> diceRoll(1, sides);
   
-  cout << "You rolled a " << diceRoll(randomGenerator) << endl;
+    cout << "You rolled a " << diceRoll(randomGenerator) << endl;
+    answering = true;
+    while(answering){
+        cout << "Roll again? (y or n)\n";
+        cin >> again;
+        if(again == 'n'){
+            run = false;
+            answering = false;
+        }else if(again == 'y'){
+            run == true;
+            answering = false;
+        }else{
+            cout << "Yes (y) or no (n)?";
+        }
+    }
+  }
 }
